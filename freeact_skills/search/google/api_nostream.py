e0d73fc143bcbc394d809e6136ccee584346c367
Martin Krasser <krasserm@googlemail.com>
2024-12-23 17:56:19 +0100

    Additional non-streaming version of Google search

--- freeact_skills/search/google/api_nostream.py
+++ freeact_skills/search/google/api_nostream.py
@@ -4,9 +4,9 @@ class InternetSearch:
     def __init__(self, api_key: str | None = None):
         self.api_key = api_key
 
-    def search(self, natural_language_query: str):
+    def search(self, natural_language_query: str) -> str:
         """
-        Search for up-to-date information on the internet and stream the result to stdout in markdown format.
+        Search for up-to-date information on the internet and return result in markdown format.
 
         Args:
             natural_language_query (str): A query string that matches a specific topic, concept, or fact.
@@ -14,4 +14,4 @@ class InternetSearch:
         """
         from freeact_skills.search.google import impl
 
-        impl.search(natural_language_query, self.api_key)
+        return impl.search(natural_language_query, self.api_key)
